f16e9eda90dbe9bd0a406ef97ef002d845937b3a
Jordan O'Leary <jdoleary@gmail.com>
2017-11-13 23:49:29 -0500

    feat (Build) Initial working build with tests

---
+++ index.js
@@ -0,0 +1,56 @@
+/*
+supported input types
+3:30 is 3:30
+15:00 is 3pm
+4pm is 4pm
+4.4 is 4 hours and 24 minutes
+*/
+
+function fuzzyTimeInput(input, returnType){
+  if(!returnType){
+    returnType = 'string';
+  }
+  var am = true;
+  // determine am or pm
+  if(input.indexOf('a') != -1){
+    am = true;
+  }else if(input.indexOf('p') != -1){
+    am = false;
+  }
+    
+  var val = {hours:0,minutes:0};
+  if(input.indexOf(':') != -1){
+    var split = input.split(':');
+    val.hours = parseInt(split[0]);
+    // if in the PM add 12 hours
+    if(!am){
+      val.hours += 12;
+    }
+    val.minutes = parseInt(split[1]);
+    return returnWithType(val,returnType);
+  }
+  if(input.indexOf('.') != -1){
+    var split = input.split('.');
+    val.hours = split[0];
+    // 60 minutes / 10
+    val.minutes = 60 * split[1]/10;
+    return returnWithType(val,returnType);
+  }
+  
+  
+}
+function returnWithType(val,type){
+  if(val.hours >= 24 || val.minutes >= 60){
+    return null;
+  }
+  switch(type){
+    case 'json':
+      return val;
+    break;
+    case 'string':
+      return val.hours + ':' + val.minutes;
+    break;
+  }
+}
+
+module.exports = fuzzyTimeInput;
---
+++ test/test.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var fuzz = require('../index');
+
+/*
+
+3:30 is 3:30
+15:00 is 3pm
+4pm is 4pm
+4.4 is 4 hours and 24 minutes
+*/
+describe('Simple Input Time Matching', function() {
+  it('##:## should convert to approprate format', function() {
+    assert.equal(fuzz('3:30'), '3:30');
+  });
+  it('am and pm work', function() {
+    assert.equal(fuzz('3:30pm'), '15:30');
+    assert.equal(fuzz('3:30am'), '3:30');
+  });
+  it('invalid times return null', function() {
+    assert.equal(fuzz('24:30pm'), null);
+  });
+  it('decimals', function() {
+    assert.equal(fuzz('4.4'), '4:24');
+  });
+});
